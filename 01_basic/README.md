70a7ba68d5fd9f6b87a18eb1f225f8f887cf5bdb
sovereignyy99 <sovereignyy@gmail.com>
2022-04-24 16:59:47 +0800

    basic usage.

---
+++ 01_basic/README.md
@@ -0,0 +1,6 @@
+# Basic
+Basic usage.
+- cmake_minimum_required
+- project
+- add_executable
+- set
---
+++ 01_basic/basic.cpp
@@ -0,0 +1,15 @@
+/**
+ * @Author: mp
+ * @Date: 2022/4/24 16:54
+ * @Last Modified by: mp
+ * @Last Modified time: 2022/4/24 16:54
+ */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, world." << std::endl;
+
+    return 0;
+}
